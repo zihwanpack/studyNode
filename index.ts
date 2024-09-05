5069ad8baae732c179b8c2f0af15a7649233c222
Layton <jihwan8807@naver.com>
2024-09-05 13:51:42 +0900

    feat : setting eslint

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('hi');
+import http from 'http';
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': ' text/html: charset=uts-8' });
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello UMC!</p>');
+  })
+  .listen(8080, () => {
+    console.log('server가 켜짐');
+  });
